55ed2d55bdfe807e7b255642188e9d294502dcea
ferdinand89 <ferdinand@koenigonline.eu>
2018-01-23 17:57:43 +0100

    started Login and Navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Navigation from './components/Navbar'
+class App extends React.Component {
+    render() {
+        return (
+           <Router>
+               <div>
+                   <Navigation/>
+                   <Route exact path="/" component={Home}/>
+                   <Route exact path="/Login" component={Login}/>
+               </div>
+           </Router>
+        );
+    }
+}
 
-const App = () => <h1>Hello</h1>
-export default App
+export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="Login">
+                <h1>Login</h1>
+                <h3>User:</h3>
+                <input/>
+                <h3>Password:</h3>
+                <input/>
+
+            </div>
+        );
+    }
+}
+
+export default Home;
