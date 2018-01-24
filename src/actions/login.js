ffafab91879506e487b0330c58b0e1e7eb322577
ferdinand89 <ferdinand@koenigonline.eu>
2018-01-24 12:39:45 +0100

    started with redux

---
+++ src/actions/login.js
@@ -0,0 +1,12 @@
+export const INIT_LOGIN ='INIT_LOGIN';
+export const DO_LOGIN ='DO_LOGIN';
+export const LOGIN_SUCCESS ='LOGIN_SUCCESS';
+export const LOGIN_ERROR='LOGIN_ERROR';
+const URL ='';
+
+export function initLogin() {
+    return {
+        type: INIT_LOGIN
+    }
+}
+
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import './style.css'
 class Home extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: null,
+            password: null
+        }
+        //this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    handleNameChange = function(event) {
+        this.setState({name: event.target.value});
+    };
+
+    handlePasswordChange = function(event) {
+        this.setState({password: event.target.value});
+    };
     render() {
-        return (
+
+        const marginLarge = {
+            marginTop: '100%'
+        };
+
+        /**
+         * Display login form
+         */
+        return(
             <div className="Login">
                 <h1>Login</h1>
-                <h3>User:</h3>
-                <input/>
-                <h3>Password:</h3>
-                <input/>
-
+                <form style={marginLarge}>
+                   <input type="text" name="email" placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange} /> <br/><hr/>
+                    <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleNameChange} /> <br/><hr/>
+                    <input type="password" name="password" placeholder="Passwort" value={this.state.password} onChange={this.handlePasswordChange} /> <br/><hr/>
+                    <button type="button" onClick={this.handleLogin}>Login</button>
+                    <button type="button" onClick={this.handleRegistry}>Registrieren</button>
+                </form>
             </div>
         );
     }
